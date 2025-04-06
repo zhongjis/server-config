9ad5f89b1ba79c7850bec849e3e856ae86b02143
zshen <zhongjie.x.shen@gmail.com>
2025-04-06 13:14:02 -0600

    add new user

--- hosts/k3s/configuration.nix
+++ hosts/k3s/configuration.nix
@@ -2,8 +2,11 @@ custHostConfig: {
   modulesPath,
   lib,
   pkgs,
+  config,
   ...
-}: {
+}: let
+  sopsFile = ../../secrets/homelab.yaml;
+in {
   imports =
     [
       (modulesPath + "/installer/scan/not-detected.nix")
@@ -32,6 +35,21 @@ custHostConfig: {
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINDkA9QW9+SBK4dXpIj9nR9k49wuPdjlMwLvSacM9ExM zhongjie.x.shen@gmail.com"
   ];
+
+  users.users."${custHostConfig.hostUser}" = {
+    isNormalUser = true;
+    extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+    # Created using mkpasswd
+    shell = pkgs.zsh;
+    # hashedPasswordFile = config.sops.secrets.server_password_sha256.path;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINDkA9QW9+SBK4dXpIj9nR9k49wuPdjlMwLvSacM9ExM zhongjie.x.shen@gmail.com"
+    ];
+  };
+  programs.zsh.enable = true;
 
   system.stateVersion = "24.05";
 }
