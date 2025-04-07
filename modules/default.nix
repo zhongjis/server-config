83f5f60727500a556ea97ffd38b47a1fac21c960
zshen <zhongjie.x.shen@gmail.com>
2025-04-06 21:06:37 -0600

    refactor

--- modules/default.nix
+++ modules/default.nix
@@ -1,61 +1,37 @@
 {
-  config,
-  lib,
-  myLib,
+  inputs,
+  pkgs,
   ...
-}: let
-  cfg = config.myNixOS;
+}: {
+  # set global nix path
+  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
 
-  # Taking all modules in ./features and adding enables to them
-  features =
-    myLib.extendModules
-    (name: {
-      extraOptions = {
-        myNixOS.${name}.enable = lib.mkEnableOption "enable my ${name} configuration";
-      };
-
-      configExtension = config: (lib.mkIf cfg.${name}.enable config);
-    })
-    (myLib.filesIn ./features);
-
-  # Taking all module bundles in ./bundles and adding bundle.enables to them
-  bundles =
-    myLib.extendModules
-    (name: {
-      extraOptions = {
-        myNixOS.bundles.${name}.enable = lib.mkEnableOption "enable ${name} module bundle";
-      };
-
-      configExtension = config: (lib.mkIf cfg.bundles.${name}.enable config);
-    })
-    (myLib.filesIn ./bundles);
-
-  # Taking all module services in ./services and adding services.enables to them
-  services =
-    myLib.extendModules
-    (name: {
-      extraOptions = {
-        myNixOS.services.${name}.enable = lib.mkEnableOption "enable ${name} service";
-      };
-
-      configExtension = config: (lib.mkIf cfg.services.${name}.enable config);
-    })
-    (myLib.filesIn ./services);
-in {
-  imports =
-    []
-    ++ features
-    ++ bundles
-    ++ services;
-
-  config = {
-    programs.nix-ld.enable = true;
-    nixpkgs.config.allowUnfree = true;
-
-    nix.gc = {
-      automatic = false; # conflict with programs.nh.clean.enable
-      dates = "daily";
-      options = "--delete-older-than 14d";
-    };
+  # caches
+  nix.settings = {
+    trusted-users = ["root" "@wheel" "zshen"];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://cuda-maintainers.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://devenv.cachix.org"
+      "https://nix-gaming.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://mitchellh-nixos-config.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "mitchellh-nixos-config.cachix.org-1:bjEbXJyLrL1HZZHBbO4QALnI5faYZppzkU4D2s0G8RQ="
+    ];
   };
+
+  nix.package = pkgs.nix;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  nix.optimise.automatic = true;
 }
