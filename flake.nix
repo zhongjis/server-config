f9d8635307af45023955aeab257b8317c1587b27
zshen <zhongjie.x.shen@gmail.com>
2025-12-16 21:39:46 -0800

    misc: remove unused code

--- flake.nix
+++ flake.nix
@@ -29,17 +29,6 @@
     myLib = import ./lib/defaults.nix {inherit inputs nixpkgs colmena disko sops-nix;};
   in
     with myLib; {
-      nixosConfigurations = {
-        homelab-0 = mkK3sNode "homelab-0" {
-          hostModule = ./hosts/k3s/configuration.nix;
-          isMaster = true;
-        };
-        homelab-1 = mkK3sNode "homelab-1" {
-          hostModule = ./hosts/k3s/configuration.nix;
-          masterAddr = "192.168.50.104";
-        };
-      };
-
       colmenaHive = colmena.lib.makeHive {
         meta = {
           nixpkgs = import nixpkgs {
