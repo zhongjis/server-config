b41579307d08ac7fe458432d6024374daf29db93
zshen <zhongjie.x.shen@gmail.com>
2025-12-16 21:39:41 -0800

    misc: remove unused code

--- lib/defaults.nix
+++ lib/defaults.nix
@@ -1,37 +1,8 @@
 {
   sops-nix,
   disko,
-  nixpkgs,
-  inputs,
   ...
 }: {
-  mkK3sNode = hostName: {
-    user ? "nixos",
-    system ? "x86_64-linux",
-    hostModule,
-    isMaster ? false,
-    masterAddr ? "",
-  }: let
-    custHostConfig = {
-      hostName = hostName;
-      hostUser = user;
-      isK3sMaster = isMaster;
-      masterAddr = masterAddr;
-    };
-  in
-    nixpkgs.lib.nixosSystem {
-      system = system;
-      specialArgs = {
-        inherit inputs hostName custHostConfig;
-      };
-
-      modules = [
-        sops-nix.nixosModules.sops
-        disko.nixosModules.disko
-        hostModule
-      ];
-    };
-
   mkNodeSpecialArgs = hostname: {
     user ? "nixos",
     isMaster ? false,
